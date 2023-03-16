53e36eee29ff0341f75b110329a2c7c30319c9ec
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-16 13:13:15 +0000

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 // reverses a list
 exports.esrever = function (list) {
-  return list.sort((a, b) => b - a);
+  for (let index = 0; index < parseInt(list.length / 2); index++) {
+    const tmp = list[list.length - 1 - index];
+    list[list.length - 1 - index] = list[index];
+    list[index] = tmp;
+  }
+  return list
 };
