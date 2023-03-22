c891349b5129932928021ee5a57b2e038e3fe66c
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-22 17:26:28 +0300

    Add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""Lists all states where name matched argument passed"""
+"""Filter states by user input"""
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
 if __name__ == "__main__":
+    # Create connection to MySQL server
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
+    # Prepare a cursor object
     cur = conn.cursor()
-    q = """SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+
+    # Use "format" method to create the SQL query with the user input
+    q = """
+        SELECT * FROM states
+        WHERE name
+        LIKE BINARY '{}'
+        ORDER BY states.id ASC
+        """
+
     q = q.format(argv[4])
+
+    # Execute the SQL query
     cur.execute(q)
+
+    # Fetch all the rows
     query_rows = cur.fetchall()
+
+    # Print the rows
     for row in query_rows:
         print(row)
+
+    # Close the cursor and database connection
     cur.close()
     conn.close()
