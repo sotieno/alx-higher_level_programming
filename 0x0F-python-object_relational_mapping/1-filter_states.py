8be029f30095bc30813f33b9fc3e31b5d67b844a
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-22 17:42:19 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,24 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    # Create connection to MySQL server
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    # Prepare a cursor object
     cur = conn.cursor()
+
+    # Execute the SQL query
     cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+
+    # Fetch all the rows
     query_rows = cur.fetchall()
+
+    # Print the rows
     for row in query_rows:
         if row[1].startswith("N"):
             print(row)
+
+    # Close the cursor and database connection
     cur.close()
     conn.close()
