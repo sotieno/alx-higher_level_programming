7087de8a87a4f403e77122ff2abd180bb2428f54
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-23 13:19:50 +0300

    Add 14-model_city_fetch_by_state.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Adds a brand new State to the system!"""
+
+# Import required modules
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+# Check if script is being executed as main program
+if __name__ == "__main__":
+    # Create a database engine using the values
+    # of the first three command line arguments
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # The schema is created in the database specified
+    # by the connection string passed to 'create_engine'
+    Base.metadata.create_all(engine)
+
+    # Create a session object to be used to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Fetch all 'City' objects from the database and print them
+    rows = session.query(City, State).\
+        filter(City.state_id == State.id).all()
+    for city, state in rows:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.commit()
+
+    # Close object and database connection
+    session.close()
