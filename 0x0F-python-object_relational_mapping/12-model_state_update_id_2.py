f303d78c73ee9756998a0eb51978e655f73a098a
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-23 12:23:24 +0300

    Add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Adds a brand new State to the system!"""
+
+# Import required modules
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+# Check if script is being executed as main program
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # The schema is created in the database specified
+    # by the connection string passed to 'create_engine'
+    Base.metadata.create_all(engine)
+
+    # Create a session object to be used to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Change the name of the State where id = 2 to New Mexico
+    states = session.query(State).filter(State.id == 2).all()
+    if states:
+        states[0].name = "New Mexico"
+    session.commit()
+
+    # Close object and database connection
+    session.close()
