59541fa4af4f08f73ced2af3b78444733299f3ad
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-03-31 13:25:31 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch as catch can
-curl -s -X PUT "0.0.0.0:5000/catch_me" -L -H "Origin: ALX" -d "user_id=98"
+curl -sL -X PUT "0.0.0.0:5000/catch_me" -H "Origin: ALX" -d "user_id=98"
